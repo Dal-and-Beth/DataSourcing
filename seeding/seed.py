42eff4221b3d07c21ec8627451d503cf413ae141
Dallin Christensen <dallinc2@gmail.com>
2023-07-16 00:43:29 -0600

    Add DB seeding and move files

---
+++ seeding/seed.py
@@ -0,0 +1,38 @@
+from deta import Deta
+import pandas as pd
+
+deta = Deta("DB TOKEN HERE")
+
+db = deta.Base("daily_affirmations_test")
+
+
+def putIntoDB(db):
+    df270 = pd.read_excel("../scraping/270Affirmations.xlsx")
+    df160 = pd.read_excel("../scraping/160Affirmations.xlsx")
+
+    df160dict = df160.to_dict("list")
+    df270dict = df270.to_dict("list")
+
+    for key in df270dict:
+        for value in df270dict[key]:
+            # print(key, value)
+            db.put({"category": key, "text": value})
+
+    for key in df160dict:
+        for value in df160dict[key]:
+            if pd.isna(value) != True:
+                # print(key, value)
+                db.put({"category": key, "text": value})
+
+
+putIntoDB(db)
+
+all_records = db.fetch()
+print(type(all_records.items))
+
+new_list = []
+for i in all_records.items:
+    if i["category"] not in new_list:
+        new_list.append(i["category"])
+
+print(new_list)  # sanity check on categories
